92f80df14a07d0db3f2a89c8a5821a690514862a
Andrey Kotlyarov <apkotl@gmail.com>
2025-07-18 02:23:07 +0300

    FastAPI. Created config.py for reading Environment variables

---
+++ api/src/api/config.py
@@ -0,0 +1,61 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, SecretStr
+from pathlib import Path
+
+BASE_DIR = str(Path(__file__).resolve().parents[3])
+
+
+# singleton decorator
+def singleton(cls):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+
+@singleton
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        #env_file="../../../.env",
+        env_file=f"{BASE_DIR}/.env",
+        env_file_encoding='utf-8',
+        extra="ignore"
+    )
+
+    #app_name: str = Field(..., env="APP_NAME")
+    APP_ENVIRONMENT: str = Field(...)
+    APP_NAME: str = Field(...)
+    APP_VERSION: str = Field(...)
+    APP_API_VERSION: str = Field(...)
+    APP_WEB_VERSION: str = Field(...)
+
+    NGINX_WEB_SERVER_NAME: str = Field()
+    NGINX_API_SERVER_NAME: str = Field()
+    NGINX_WEB_HTTP_PROTOCOL: str = Field()
+    NGINX_API_HTTP_PROTOCOL: str = Field()
+    NGINX_WEB_PORT: str = Field()
+    NGINX_API_PORT: str = Field()
+
+    POSTGRES_DB: str = Field()
+    POSTGRES_USER: str = Field()
+    POSTGRES_PASSWORD: str = Field()
+    POSTGRES_PORT: str = Field()
+
+    base_dir: str = Field(BASE_DIR)
+
+
+# Создаем экземпляр настроек, который будет использоваться во всем приложении
+settings = Settings()
+
+if __name__ == "__main__":
+    print(f"base_dir: {settings.base_dir}")
+    print(f"APP_ENVIRONMENT: {settings.APP_ENVIRONMENT}")
+    print(f"APP_NAME: {settings.APP_NAME}")
+    print(f"APP_VERSION: {settings.APP_VERSION}")
+    print(f"APP_API_VERSION: {settings.APP_API_VERSION}")
+    print(f"APP_WEB_VERSION: {settings.APP_WEB_VERSION}")
+    #print(f"NGINX_WEB_SERVER_NAME: {settings.NGINX_WEB_SERVER_NAME}")
--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .config import settings
+
 # Create Application
 app = FastAPI()
 
 # CORS settings
 origins = [
-    "http://localhost",        # Для локальной разработки вашего сайта
-    "http://localhost:5173",   # Если ваш фронтенд работает на другом порту
-    "http://your_domain.com",  # Для продакшен домена вашего сайта
-    "http://www.your_domain.com" # Если используется www
-    # "https://your_domain.com", # Если используете HTTPS
+    f"{settings.NGINX_WEB_HTTP_PROTOCOL}://{settings.NGINX_WEB_SERVER_NAME}",
+
+    #"http://localhost",
+    #"http://localhost:5173",
+    #"http://your_domain.com",
+    #"http://www.your_domain.com",
+    #"https://your_domain.com",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"], # Разрешить все методы (GET, POST, PUT, DELETE и т.д.)
-    allow_headers=["*"], # Разрешить все заголовки
+    allow_methods=["*"], # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"], # Allow all headers
 )
 
 @app.get("/")
@@ -28,13 +32,39 @@ def read_root():
 
 @app.get("/version")
 def read_item(q: str | None = None):
-    return {"data": {"version": "0.0.2", "q": q}}
+    return {"data": {"version": settings.APP_API_VERSION, "q": q}}
+
+
+@app.get("/env")
+def read_item():
+    return {
+        "data":
+            {
+                "base_dir": f"{settings.base_dir}",
+                "APP_ENVIRONMENT": settings.APP_ENVIRONMENT,
+
+                "APP_NAME": settings.APP_NAME,
+                "APP_VERSION": settings.APP_VERSION,
+                "APP_API_VERSION": settings.APP_API_VERSION,
+                "APP_WEB_VERSION": settings.APP_WEB_VERSION,
+
+                "NGINX_API_SERVER_NAME": settings.NGINX_API_SERVER_NAME,
+                "NGINX_API_HTTP_PROTOCOL": settings.NGINX_API_HTTP_PROTOCOL,
+                "NGINX_API_PORT": settings.NGINX_API_PORT,
+
+                "NGINX_WEB_SERVER_NAME": settings.NGINX_WEB_SERVER_NAME,
+                "NGINX_WEB_HTTP_PROTOCOL": settings.NGINX_WEB_HTTP_PROTOCOL,
+                "NGINX_WEB_PORT": settings.NGINX_WEB_PORT,
+
+                "all_settings": settings if settings.APP_ENVIRONMENT == "dev" else "only available for 'dev' of the environment"
+            }
+    }
 
 
 @app.get("/items/{item_count}")
 def read_item(item_count: int, prefix: str | None = "item_#"):
     return {
-        "data": {"version": "0.0.2", "item_count": item_count, "prefix": prefix},
+        "data": {"version": settings.APP_API_VERSION, "item_count": item_count, "prefix": prefix},
         "items": [item for item in simple_item_generator(item_count, prefix)]
     }
 
